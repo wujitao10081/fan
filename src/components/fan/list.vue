eab4bded735aeedd96914f0e0e19c3ba8ece3f81
fan <945228350@qq.com>
2021-03-02 16:14:52 +0800

    时间戳，排序

--- src/components/fan/list.vue
+++ src/components/fan/list.vue
@@ -2,21 +2,33 @@
     <div>
         <table border="1px">
             <tr>
-                <th v-for="(item,index) in titleList" :key="index">{{item}}</th>
+                <th>{{this.titleList.id}}</th>
+                <th>{{this.titleList.title}}</th>
+                <th>{{this.titleList.num}}</th>
+                <th>{{this.titleList.time}}</th>
+                <th>{{this.titleList.level}}</th>
             </tr>
             <tr v-for="(itemSeller,index) in seller" :key="index">
-                <td >{{itemSeller.time}}</td>
+                <td >{{itemSeller.event_id}}</td>
                 <td >{{itemSeller.title}}</td>
                 <td >{{itemSeller.count}}</td>
-                <td >{{itemSeller.event_id}}</td>
+                <td >{{itemSeller.time | formatDate}}</td>
                 <td >{{itemSeller.level}}</td>
-                
             </tr>
         </table>
     </div>
 </template>
 
 <script>
+import moment from 'moment'
+// function changedate(shijianchuo) {
+//     var now = new Date(shijianchuo),
+//     y = now.getFullYear(),
+//     m = ("0" + (now.getMonth() + 1)).slice(-2),
+//     d = ("0" + now.getDate()).slice(-2);
+//     return y + "-" + m + "-" + d + " " + now.toTimeString().substr(0, 8);
+// }
+     
 export default {
     name:'list',
     data() {
@@ -28,13 +40,23 @@ export default {
     mounted() {
         this.axios.get("/api/titleList").then((response) => {
             console.log(response.data.data.data)
-            this.titleList = response.data.data.data      
+            this.titleList = response.data.data.data     
 })
         this.axios.get("/api/seller").then((res) => {
-        console.log(res.data.data)
-        this.seller = res.data.data   
-         } )
-}}
+            console.log(res.data.data);
+            this.seller = res.data.data 
+            this.seller.sort((a,b)=>{
+                return a.event_id-b.event_id
+            })          
+     } )     
+        
+ },     
+    filters:{    
+      formatDate: function () {
+        return moment().format('YYYY-MM-DD')
+      }
+    }
+}
 </script>
 
 <style scoped>
